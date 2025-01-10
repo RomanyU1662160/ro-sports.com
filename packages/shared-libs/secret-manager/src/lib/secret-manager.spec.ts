fefeb6a44001b8356e7577d3c597cc2185492712
Romany <romanyfayiez@gmail.com>
2025-01-10 22:37:44 +0000

    Add secret manager tests

--- packages/shared-libs/secret-manager/src/lib/secret-manager.spec.ts
+++ packages/shared-libs/secret-manager/src/lib/secret-manager.spec.ts
@@ -1,7 +1,61 @@
-import { secretManager } from './secret-manager';
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from '@aws-sdk/client-secrets-manager';
+import { BaseSecretManager } from './secret-manager';
+import { mockClient } from 'aws-sdk-client-mock';
 
-describe('secretManager', () => {
-  it('should work', () => {
-    expect(secretManager()).toEqual('secret-manager');
+type Test = {
+  test: string;
+};
+class TestSecretManager extends BaseSecretManager<Test> {
+  getSecretName = () => {
+    return 'test';
+  };
+
+  validateSecret = (secret: Test) => {
+    return typeof secret.test === 'string';
+  };
+}
+
+describe('BaseSecretManager', () => {
+  let testSecretManager: TestSecretManager;
+  const mockSecretManager = mockClient(SecretsManagerClient);
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    mockSecretManager.reset();
+    testSecretManager = new TestSecretManager();
+  });
+
+  it('should fetch and cache secret', async () => {
+    mockSecretManager.reset();
+    const mockSecret = { test: 'value' };
+    mockSecretManager.on(GetSecretValueCommand).resolves({
+      SecretString: JSON.stringify(mockSecret),
+    });
+
+    const result = await testSecretManager.getSecret();
+    expect(result).toEqual(mockSecret);
+
+    // Should use cache on second call
+    const cachedResult = await testSecretManager.getSecret();
+    expect(cachedResult).toEqual(mockSecret);
+    expect(mockSecretManager.commandCalls(GetSecretValueCommand)).toHaveLength(
+      1
+    );
+  });
+
+  it('should throw an error if the secret name is not configured in the .env file', async () => {
+    testSecretManager.getSecretName = jest.fn().mockReturnValueOnce('');
+    await expect(testSecretManager.getSecret()).rejects.toThrow();
+  });
+
+  it('should throw error for invalid secret', () => {
+    mockSecretManager
+      .on(GetSecretValueCommand)
+      .resolvesOnce({ SecretString: JSON.stringify({ invalid: 'test' }) });
+
+    expect(testSecretManager.getSecret()).rejects.toThrow();
   });
 });
--- packages/shared-libs/secret-manager/src/lib/secret-manager.ts
+++ packages/shared-libs/secret-manager/src/lib/secret-manager.ts
@@ -1,1 +1,99 @@
-export abstract class SecretManager {}
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from '@aws-sdk/client-secrets-manager';
+
+export abstract class BaseSecretManager<T> {
+  private readonly client: SecretsManagerClient;
+  private readonly ttl: number; // Time to live in hours
+  private cache: Map<string, { secret: T; timestamp: number }>;
+
+  protected abstract getSecretName(): string;
+  protected abstract validateSecret(secret: T): boolean;
+
+  constructor(ttl = 1) {
+    this.cache = new Map();
+    this.ttl = ttl * 60 * 60 * 1000; // Convert hours to milliseconds
+    try {
+      this.client = new SecretsManagerClient({
+        region: process.env.AWS_REGION || 'eu-west-1',
+      });
+    } catch (error) {
+      console.log('error:::>>>', error);
+      throw error;
+    }
+  }
+
+  async getSecret(forceRefresh = false): Promise<T> {
+    const secretName = this.getSecretName();
+    console.log('secretName:::>>>', secretName);
+    try {
+      if (!secretName) {
+        console.log(
+          'Secret name is not configured in your .env file',
+          secretName
+        );
+        throw new Error(
+          `Secret name: ${secretName},  is not configured in your .env file`
+        );
+      }
+      const cachedSecret = this.cache.get(secretName);
+      console.log('cachedSecret:::>>>', cachedSecret);
+
+      const isCached = this.isCacheValid(secretName);
+      console.log('isCached:::>>>', isCached);
+
+      if (!forceRefresh && isCached && cachedSecret) {
+        console.log('Using cached secret');
+        return JSON.parse(JSON.stringify(cachedSecret.secret)) as T;
+      }
+      console.log('retrieving secret from AWS Secrets Manager');
+      const command = new GetSecretValueCommand({
+        SecretId: secretName,
+      });
+
+      const response = await this.client.send(command);
+      console.log('response:::>>>', response);
+
+      const secretValue = response.SecretString;
+      if (!secretValue) {
+        console.log(
+          'Secret value is empty or not found in AWS Secrets Manager'
+        );
+        throw new Error(
+          'Secret value is empty or not found in AWS Secrets Manager'
+        );
+      }
+
+      const parsedSecret = JSON.parse(secretValue) as T;
+      console.log('parsedSecret:::>>>', parsedSecret);
+      if (!this.validateSecret(parsedSecret)) {
+        console.log('Secret is invalid');
+        throw new Error('Secret validation failed, invalid secret format');
+      }
+
+      // store the secret in the cache
+      this.cache.set(secretName, {
+        secret: parsedSecret,
+        timestamp: Date.now(),
+      });
+
+      return parsedSecret;
+    } catch (error) {
+      console.log('error retrieving Secret:::>>>', error);
+      throw error;
+    }
+  }
+
+  private isCacheValid(secretName: string) {
+    const cachedSecret = this.cache.get(secretName);
+    if (!cachedSecret) return false;
+
+    const now = Date.now();
+    return now - cachedSecret.timestamp < this.ttl;
+  }
+
+  private clearCache() {
+    this.cache.clear();
+  }
+}
