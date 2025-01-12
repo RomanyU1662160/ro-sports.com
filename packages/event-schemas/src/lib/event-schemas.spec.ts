d00d88a7504499d9ae660307edc55da4e0c009cf
Romany <romanyfayiez@gmail.com>
2025-01-12 22:58:10 +0000

    add event-schemas

---
+++ packages/event-schemas/src/lib/event-schemas.spec.ts
@@ -0,0 +1,7 @@
+import { eventSchemas } from './event-schemas';
+
+describe('eventSchemas', () => {
+  it('should work', () => {
+    expect(eventSchemas()).toEqual('event-schemas');
+  });
+});
---
+++ packages/platform/src/backend/main.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "remote_state" {
+  bucket = var.s3_bucket_name
+  tags = {
+    Environment = var.environment
+    project     = var.project_name
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Environment = var.environment
+    project     = var.project_name
+    ManagedBy   = "Terraform"
+  }
+
+}
+
+
